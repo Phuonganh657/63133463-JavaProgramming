16eb0c6776840110b3919f1e5f8259c5fe4d6004
Phuonganh657 <146166561+Phuonganh657@users.noreply.github.com>
2024-01-23 10:08:51 +0700

    Ví dụ hello

---
+++ Bai1_Hello/src/SayHelloApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao. Toi la cac SV khoa 63 CNTT - NTU: Hoc lap trinh Java");
+		System.out.print("Tran Ngoc Phuong Anh");
+
+	}
+
+}
