048f7021d93e69aa88233f9c0fda0c43551e0517
Trần Ngọc Phương Anh <146166561+Phuonganh657@users.noreply.github.com>
2024-02-26 19:33:04 +0700

    lab4bai1

---
+++ Lab4/src/bai1/SanPham.java
@@ -0,0 +1,50 @@
+package bai1;
+
+import java.util.Scanner;
+
+public class SanPham {
+    // Khai báo các thuộc tính
+    private String tenSp;
+    private double donGia;
+    private double giamGia;
+
+    public SanPham(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    // Phương thức tính thuế nhập khẩu
+    public double getThueNhapKhau() {
+        return 0.1 * donGia;
+    }
+
+    // Phương thức xuất thông tin ra màn hình
+    public void xuat() {
+        System.out.println("Tên sản phẩm: " + tenSp);
+        System.out.println("Đơn giá: " + donGia);
+        System.out.println("Giảm giá: " + giamGia);
+        System.out.println("Thuế nhập khẩu: " + getThueNhapKhau());
+    }
+
+    // Phương thức nhập thông tin từ bàn phím
+    public void nhap() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên sản phẩm: ");
+        this.tenSp = scanner.nextLine();
+        System.out.print("Nhập đơn giá: ");
+        this.donGia = scanner.nextDouble();
+        System.out.print("Nhập giảm giá: ");
+        this.giamGia = scanner.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        SanPham sp = new SanPham("Sản phẩm 1", 100, 10);
+        sp.xuat();
+
+       
+        SanPham sp2 = new SanPham("", 0, 0);
+        sp2.nhap();
+        sp2.xuat();
+    }
+}
