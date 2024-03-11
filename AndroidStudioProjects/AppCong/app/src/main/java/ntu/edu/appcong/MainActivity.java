70aa6ed6d4d02868d35131ce4870eb95b9fe101d
Trần Ngọc Phương Anh <146166561+Phuonganh657@users.noreply.github.com>
2024-03-12 00:27:30 +0700

    vd3

---
+++ AndroidStudioProjects/AppCong/app/src/main/java/ntu/edu/appcong/MainActivity.java
@@ -0,0 +1,46 @@
+package ntu.edu.appcong;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    //Đây là bộ lắng nghe va xử lý sự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view){
+        // tìm tham chiếu đến điều khiển trên tep XML
+        EditText editTextSoA =     findViewById(R.id.edtA);
+        EditText  editTextSoB =     findViewById(R.id.edtB);
+        EditText  editTextKetQua =     findViewById(R.id.edtKQ);
+
+        //lấy dữ liệu về ở điều khiển so a
+        String strA =    editTextSoA.getText().toString();
+        String strB =    editTextSoB.getText().toString();
+
+        //Chuyển dữ liệu sang dạng số
+        int so_A= Integer.parseInt(strA);
+        int so_B= Integer.parseInt(strB);
+
+        //Tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+
+        //hiện ra màn hình
+        editTextKetQua.setText(strTong);
+    }
+}
